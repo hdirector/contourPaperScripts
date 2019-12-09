75e8e4bd2ff9a9b07768cbcff73e6fad9e8d71ca
Hannah <direch@uw.edu>
2019-12-08 17:10:52 -0800

    figure labels added and table results

--- figure_code/star_shaped_metrics.R
+++ figure_code/star_shaped_metrics.R
@@ -2,6 +2,7 @@ rm(list = ls())
 #TO DO: ADD FINDING THE OPTIMAL C EACH EXAMPLE
 library("ContouR")
 library("maptools")
+library("xtable")
 p <- 500
 theta <- seq(0, 2*pi, length = p)
 
@@ -20,8 +21,9 @@ curl <- assess_star(cont = curl_poly, C = c(.5, .5),theta = theta)
 
 ###########################
 #make figure
-##########################
-par(mfrow = c(4, 3))
+#########################
+pdf("/users/hdirector/desktop/starShapedDemo.pdf")
+par(mfrow = c(4, 3), oma = rep(.2, 4), mar = c(.2, .2, 4, .2))
 #ex 1
 plot(cp_poly)
 plot(cp_poly)
@@ -57,3 +59,25 @@ plot(curl$u_approx, add = T, border = 'red')
 plot(curl_poly)
 plot(curl$o_error, add = T, col = 'lightblue')
 plot(curl$o_approx, add = T, border = 'blue')
+
+mtext(text = "Contour", side = 3, line = -3, at = .18, outer = TRUE)
+mtext(text = "Under Approx \n Error Area", side = 3, line = -3, at = .5, 
+      outer = TRUE)
+mtext(text = "Over Approx. \n Error Area", side = 3, line = -3, at = .82,
+      outer = TRUE)
+mtext(text = "Contour 1", side = 2, line = -3, at = .85, outer = TRUE)
+mtext(text = "Contour 2", side = 2, line = -3, at = .58, outer = TRUE)
+mtext(text = "Contour 3", side = 2, line = -3, at = .35, outer = TRUE)
+mtext(text = "Contour 4", side = 2, line = -3, at = .1, outer = TRUE)
+dev.off()
+
+######################
+#table of areas
+#####################
+prop_areas <- matrix(nrow = 4, ncol = 2, byrow = TRUE,
+                      data = c(cp$u_area/cp$tot_area, cp$o_area/cp$tot_area,
+                               cm$u_area/cm$tot_area, cm$o_area/cm$tot_area,
+                               circl$u_area/circl$tot_area, circl$o_area/circl$tot_area,
+                               curl$u_area/curl$tot_area, curl$o_area/circl$tot_area))
+per_areas <- 100*prop_areas
+xtable(per_areas, digits = 3)
