52fe7faa757637a3c1747954fa3ba6779d45e521
Hannah <direch@uw.edu>
2020-01-29 11:13:14 -0800

    switching to a general runSim script from a specific script for nObsnGen

--- Simulations/runSim.R
+++ Simulations/runSim.R
@@ -5,8 +5,12 @@ library("ContouR")
 
 #Read in arguments
 args <- commandArgs(trailingOnly =TRUE)
-task_id <- as.numeric(args[1])
-task_path <- args[2]
+task_id <-as.numeric(args[1])
+task_id <- 8#11
+#task_path <- args[2]
+task_path <- "/users/hdirector/Dropbox/Contours/ContourPaperScripts/Simulations/variedPTaskTable.rda"
+#task_name <- args[3]
+task_name <- "variedP"
 load(task_path)
 task <- task_table[task_id,]
 print(sprintf("task_id: %i", task_id))
@@ -19,10 +23,11 @@ n_cred <- length(cred_levels)
 p_test <- task$p_test
 n_evals <- task$n_evals 
 n_grid <- task$n_grid
+n_C_poss <- task$n_C_poss
+p_prop <- task$p_prop
 
 #true parameters
 pars <- get(as.character(task$shape_name))
-p <- length(pars$mu)
 mu_true <- pars$mu
 p <- length(mu_true)
 kappa_true <- pars$kappa
@@ -31,13 +36,17 @@ Cx_true <- pars$Cx
 Cy_true <- pars$Cy
 thetas_true <- pars$theta
 
+
+#select the number of p's to evaluate
+p_fit <- floor(p_prop*p)
+
 #priors
-mu0_indiv <- task$mu0
-mu0 <- rep(mu0_indiv, p)
+mu0_indiv <- task$mu0 
+mu0 <- rep(mu0_indiv, p_fit)
 Lambda0_sigma2 <- task$Lambda0_sigma2
-Lambda0 <- Lambda0_sigma2*diag(p) 
-betaKappa0 <- task$betaKappa0
-betaSigma0 <- task$betaSigma0
+Lambda0 <- Lambda0_sigma2*diag(p_fit) 
+betaKappa0 <- task$betaKappa0 
+betaSigma0 <- rep(task$betaSigma0, p_fit)
 
 #MCMC settings
 n_iter <- task$n_iter
@@ -46,51 +55,64 @@ sigmaProp_sigma2 <- task$sigmaProp_sigma2
 muProp_sigma2 <- task$muProp_sigma2
 kappaPropSD <- task$kappaProp_SD
 g_space <- task$g_space
-g_space <- 1
-g_start <- seq(1, p, by = g_space)
-g_end <- c(seq(g_space, p, by = g_space), p)
+g_start <- seq(1, p_fit, by = g_space)
+g_end <- c(seq(g_space, p_fit, by = g_space), p)
 
 #sample test contours from the truth for later testing
+box <- bbox()
+bd <- box@polygons[[1]]@Polygons[[1]]@coords
 test <- gen_conts(n_sim = n_evals, mu = mu_true, kappa = kappa_true,
                   sigma = sigma_true, Cx = Cx_true, Cy = Cy_true,
-                  thetas = thetas_true)
+                  thetas = thetas_true, bd = bd)
+
+#grid of points for possible C
+x_pts <- y_pts <- seq(0, 1, length = n_C_poss)
+C_poss <- SpatialPoints(expand.grid(x_pts, y_pts))
+
+#set up thetas
+theta_space <- 2*pi/p_fit
+thetas <- seq(theta_space, 2*pi, theta_space)
+stopifnot(length(thetas) == p_fit)
 
 for (k in 1:n_evals) {
   start_time <- proc.time()  
   #simulate observations
   obs <- gen_conts(n_sim = n_obs, mu = mu_true, kappa = kappa_true,
                    sigma = sigma_true, Cx = Cx_true, Cy = Cy_true,
-                   thetas = thetas_true)
+                   thetas = thetas_true, bd = bd)
   
-  #find center point and associated angles
-  C_est <- find_center(coords = obs$coords)
-  thetas_all <- apply(obs$coords, 3, function(x){calc_angs(C = C_est,
-                                                           coords = x)})
-  stopifnot(max(apply(thetas_all, 1, sd)) <= 1e-4)
-  thetas_est <- apply(thetas_all, 1, mean)
+  #find "best" center point 
+  #C_est <- best_C(C_poss, conts = obs$polys, thetas)
+  C_est <- c(Cx_true, Cy_true)
   
   #measure and store y
-  temp <- XToWY(Cx = C_est[1], Cy = C_est[2], x = obs$coords, thetas_est)
-  y <- temp$y
+  #Make sets of lines, l, for  C_hat and get y's
+  l_untrim <- make_l(C = C_est, thetas)
+  l <- lapply(l_untrim, function(x){gIntersection(x, box)})
+  l_lengths <- sapply(l, function(x){as.numeric(gLength(x))})
+  
+  #compute y's
+  pts <- lapply(obs$polys, function(x) {pts_on_l(l, x, under = FALSE)})
+  y <- sapply(pts, function(x){apply(x, 1, function(y){get_dist(y, C_est)})})
   
   #initial values for MCMC
-  mu_ini <- apply(temp$y, 1, mean)
-  kappa_ini = 5
-  sigma_ini <-  apply(temp$y, 1, sd)
+  mu_ini <- apply(y, 1, mean)
+  kappa_ini <- 1
+  sigma_ini <-  apply(y, 1, sd)
   
   #non-scaler proposals for MCMC
-  theta_dist_est <- theta_dist_mat(thetas_est)
+  theta_dist_est <- theta_dist_mat(thetas)
   sigmaPropCov = sigmaProp_sigma2*compSigma(sigma_ini, kappa_ini, theta_dist_est)
   muPropCov <- muProp_sigma2*compSigma(sigma_ini, kappa_ini, theta_dist_est)
   
-  #run MCMC 
-  fits <- RunMCMC(nIter = n_iter, y,
-                  mu = mu_ini, mu0, Lambda0, muPropCov,
+  #run MCMC
+  fits <- ContouR::RunMCMC(nIter = n_iter, y,
+                  mu = mu_ini, mu0 = mu0, Lambda0, muPropCov,
                   kappa = kappa_ini, betaKappa0, kappaPropSD,
-                  sigma = sigma_ini, betaSigma0, sigmaPropCov,
-                  gStart = g_start - 1, gEnd = g_end - 1, 
+                  sigma = sigma_ini, betaSigma0 = betaSigma0, sigmaPropCov,
+                  gStart = g_start - 1, gEnd = g_end - 1,
                   thetaDist = theta_dist_est)
-  
+
   #parameter estimates
   mu_est <- apply(fits$mu[,(burn_in + 1):n_iter], 1, mean)
   kappa_est <- mean(fits$kappa[(burn_in + 1):n_iter])
@@ -99,33 +121,29 @@ for (k in 1:n_evals) {
   #posterior field
   gens <- gen_conts(n_sim = n_gen, mu = mu_est, kappa = kappa_est,
                     sigma = sigma_est, Cx = C_est[1], Cy = C_est[2],
-                    thetas = thetas_est)
+                    thetas = thetas, bd = bd)
   prob <- prob_field(gens$polys, nrows = n_grid, ncols = n_grid)
   
   #find credible intervals and compute coverage
   creds <- cred_regs(prob, cred_eval = cred_levels, nrows = n_grid, 
                      ncols = n_grid)
-  
-  #compute test to current grid
-  # test_k <- conv_to_poly(conv_to_grid(test$polys[[k]], nrows = n_grid, 
-  #                                   ncols = n_grid, xmn = .3, xmx = .7, 
-  #                                   ymn = .3,  ymx = .7))
+  par(mfrow = c(1, 3))
   if (k != 1) {
     cover <- cover + sapply(creds, 
-                            function(x){eval_cred_reg(truth = test_k,
+                            function(x){eval_cred_reg(truth = test$polys[[k]],
                                                       cred_reg = x, 
                                                       center = c(Cx_true, Cy_true), 
                                                       p_test = p_test,
-                                                      nrows = n_grid, 
-                                                      ncols = n_grid)})
+                                                      nrows = n_grid, ncols = n_grid,
+                                                      plotting = TRUE)})
   } else {
     cover <- sapply(creds, 
-                    function(x){eval_cred_reg(truth = test_k,
+                    function(x){eval_cred_reg(truth = test$polys[[k]],
                                               cred_reg = x, 
                                               center = c(Cx_true, Cy_true), 
                                               p_test = p_test,
-                                              nrows = n_grid, 
-                                              ncols = n_grid, plotting = TRUE)})
+                                              nrows = n_grid, ncols = n_grid,
+                                              plotting = TRUE)})
   }
   end_time <- proc.time()
   elapse_time <- end_time  - start_time
@@ -136,5 +154,5 @@ for (k in 1:n_evals) {
 #save results
 res_cover <- list("task" = task, "cover" = cover)
 save(res_cover, 
-     file = sprintf("/homes/direch/contours/Simulations/sim_results/nObsnGen/task%iShape%snObs%inGen%i.rda",
-                    task_id, task$shape_name, n_obs, n_gen))
+     file = sprintf("/homes/direch/contours/Simulations/sim_results/%s/task%iShape%snObs%inGen%i%f.rda",
+                    task_name, task_id, task$shape_name, n_obs, n_gen, p_prop))
--- Simulations/runSimLocal.R
+++ Simulations/runSimLocal.R
@@ -4,9 +4,10 @@ set.seed(103)
 library("ContouR")
 
 #Read in arguments
-#args <- commandArgs(trailingOnly =TRUE)
-task_id <- 19# as.numeric(args[1])
-task_path <- "/Users/hdirector/Dropbox/Contours/ContourPaperScripts/Simulations/nObsnGenTaskTable.rda"
+task_id <- 1
+# task_path <- "/Users/hdirector/Dropbox/Contours/ContourPaperScripts/Simulations/variedPTaskTable.rda"
+# task_path <- "/Users/hdirector/Dropbox/Contours/ContourPaperScripts/Simulations/nObsnGenTaskTable.rda"
+task_path <- "/Users/hdirector/Dropbox/Contours/ContourPaperScripts/Simulations/misspecTaskTable.rda"
 load(task_path)
 task <- task_table[task_id,]
 print(sprintf("task_id: %i", task_id))
@@ -17,27 +18,38 @@ n_gen <- task$n_gen
 cred_levels <- c(80, 90, 95)
 n_cred <- length(cred_levels)
 p_test <- task$p_test
-n_evals <- 10 #task$n_evals 
-n_grid <- 100 #task$n_grid
+n_evals <- task$n_evals 
+n_grid <- task$n_grid
+n_C_poss <- task$n_C_poss
+p_prop <- task$p_prop
+misspec <- TRUE
+r1_min <- task$r1_min
+r1_max <- task$r1_max
+r2_min <- task$r2_min
+r2_max <- task$r2_max
+n_curl_min <- task$n_curl_min
+n_curl_max <- task$n_curl_max
 
 #true parameters
 pars <- get(as.character(task$shape_name))
-p <- length(pars$mu)
 mu_true <- pars$mu
 p <- length(mu_true)
-kappa_true <- .1 # pars$kappa
+kappa_true <- pars$kappa
 sigma_true <- pars$sigma
 Cx_true <- pars$Cx
 Cy_true <- pars$Cy
 thetas_true <- pars$theta
 
+#select the number of p's to evaluate
+p_fit <- floor(p_prop*p)
+
 #priors
-mu0_indiv <- .3#task$mu0
-mu0 <- rep(mu0_indiv, p)
-Lambda0_sigma2 <- .05#task$Lambda0_sigma2
-Lambda0 <- Lambda0_sigma2*diag(p) 
-betaKappa0 <- 200 #task$betaKappa0
-betaSigma0 <- rep(task$betaSigma0, p)
+mu0_indiv <- task$mu0
+mu0 <- rep(mu0_indiv, p_fit)
+Lambda0_sigma2 <- task$Lambda0_sigma2
+Lambda0 <- Lambda0_sigma2*diag(p_fit) 
+betaKappa0 <- task$betaKappa0
+betaSigma0 <- rep(task$betaSigma0, p_fit)
 
 #MCMC settings
 n_iter <- task$n_iter
@@ -47,36 +59,51 @@ muProp_sigma2 <- task$muProp_sigma2
 kappaPropSD <- task$kappaProp_SD
 g_space <- task$g_space
 g_space <- 1
-g_start <- seq(1, p, by = g_space)
-g_end <- c(seq(g_space, p, by = g_space), p)
+g_start <- seq(1, p_fit, by = g_space)
+g_end <- c(seq(g_space, p_fit, by = g_space), p)
 
 #sample test contours from the truth for later testing
 box <- bbox()
 bd <- box@polygons[[1]]@Polygons[[1]]@coords
-test <- gen_conts(n_sim = n_evals, mu = mu_true, kappa = kappa_true,
-                  sigma = sigma_true, Cx = Cx_true, Cy = Cy_true,
-                  thetas = thetas_true, bd = bd)
+if (misspec) {
+  test <- gen_misspec(n_sim = n_evals, mu = mu_true, kappa = kappa_true,
+                    sigma = sigma_true, Cx = Cx_true, Cy = Cy_true,
+                    thetas = thetas_true, r1_min = r1_min, r1_max = r1_max,
+                    r2_min = r2_min, r2_max = r2_max, n_curl_min = n_curl_min,
+                    n_curl_max = n_curl_max, bd = bd)
+} else {
+  test <- gen_conts(n_sim = n_evals, mu = mu_true, kappa = kappa_true,
+                    sigma = sigma_true, Cx = Cx_true, Cy = Cy_true,
+                    thetas = thetas_true, bd = bd)
+}
 
 #grid of points for possible C
-n_C_poss <- 20
 x_pts <- y_pts <- seq(0, 1, length = n_C_poss)
 C_poss <- SpatialPoints(expand.grid(x_pts, y_pts))
 
-
 #set up thetas
-theta_space <- 2*pi/p
-thetas <- seq(theta_space, 2*pi, theta_space)
+theta_space <- 2*pi/p_fit
+thetas <- seq(theta_space/2, 2*pi, theta_space)
+stopifnot(length(thetas) == p_fit)
 
 for (k in 1:n_evals) {
   start_time <- proc.time()  
   #simulate observations
-  obs <- gen_conts(n_sim = n_obs, mu = mu_true, kappa = kappa_true,
-                   sigma = sigma_true, Cx = Cx_true, Cy = Cy_true,
-                   thetas = thetas_true, bd = bd)
+  if (misspec) {
+    obs <- gen_misspec(n_sim = n_obs, mu = mu_true, kappa = kappa_true,
+                     sigma = sigma_true, Cx = Cx_true, Cy = Cy_true,
+                     thetas = thetas_true, r1_min = r1_min, r1_max = r1_max,
+                     r2_min = r2_min, r2_max = r2_max, n_curl_min = n_curl_min,
+                     n_curl_max = n_curl_max, bd = bd)
+  } else {
+    obs <- gen_conts(n_sim = n_obs, mu = mu_true, kappa = kappa_true,
+                    sigma = sigma_true, Cx = Cx_true, Cy = Cy_true,
+                    thetas = thetas_true, bd = bd)
+  }
   
   #find "best" center point 
   C_est <- best_C(C_poss, conts = obs$polys, thetas)
-  
+
   #measure and store y
   #Make sets of lines, l, for  C_hat and get y's
   l_untrim <- make_l(C = C_est, thetas)
@@ -89,7 +116,7 @@ for (k in 1:n_evals) {
   
   #initial values for MCMC
   mu_ini <- apply(y, 1, mean)
-  kappa_ini <- 1
+  kappa_ini <- .1
   sigma_ini <-  apply(y, 1, sd)
   
   #non-scaler proposals for MCMC
@@ -114,18 +141,20 @@ for (k in 1:n_evals) {
   gens <- gen_conts(n_sim = n_gen, mu = mu_est, kappa = kappa_est,
                     sigma = sigma_est, Cx = C_est[1], Cy = C_est[2],
                     thetas = thetas, bd = bd)
-  prob <- prob_field(gens$polys, nrows = n_grid, ncols = n_grid)
+  prob <- prob_field(polys = gens$polys, nrows = n_grid, ncols = n_grid)
   
   #find credible intervals and compute coverage
   creds <- cred_regs(prob, cred_eval = cred_levels, nrows = n_grid, 
                      ncols = n_grid)
+  par(mfrow = c(1, 3))
   if (k != 1) {
     cover <- cover + sapply(creds, 
                             function(x){eval_cred_reg(truth = test$polys[[k]],
                                                       cred_reg = x, 
                                                       center = c(Cx_true, Cy_true), 
                                                       p_test = p_test,
-                                                      nrows = n_grid, ncols = n_grid)})
+                                                      nrows = n_grid, ncols = n_grid,
+                                                      plotting = TRUE)})
   } else {
     cover <- sapply(creds, 
                     function(x){eval_cred_reg(truth = test$polys[[k]],
@@ -144,5 +173,5 @@ for (k in 1:n_evals) {
 #save results
 res_cover <- list("task" = task, "cover" = cover)
 save(res_cover, 
-     file = sprintf("/homes/direch/contours/Simulations/sim_results/nObsnGen/task%iShape%snObs%inGen%i.rda",
+     file = sprintf("/homes/direch/contours/Simulations/sim_results/variedP/task%iShape%snObs%inGen%i.rda",
                     task_id, task$shape_name, n_obs, n_gen))
