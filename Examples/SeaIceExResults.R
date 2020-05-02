605e558234cf497129decafbdc5bb650be2fd4f2
Hannah <direch@uw.edu>
2020-05-02 12:28:43 -0700

    adjusted for final p

--- Examples/SeaIceExResults.R
+++ Examples/SeaIceExResults.R
@@ -1,14 +1,16 @@
-rm(list = ls())
-years <- 2010:2017
-p <- 66
+years <- 2008:2017
+n_years <- length(years)
+p <- 71
 cover_all <- matrix(nrow = p, ncol = 3, data = 0)
-for (y in years) {
-  load(sprintf("/Users/hdirector/Desktop/cover_year%i.rda", y))
+for (year in years) {
+  load(sprintf("/Users/hdirector/Dropbox/Contours/ContourPaperScripts/Examples/ex_results/cover_year%i.rda", year))
   cover_all <- cover_all + cover
 }
 
+
 df <- data.frame("nominal" = c(.8, .9, .95),
-                "mean" = apply(cover_all/8, 2, mean),
-                "sd" = apply(cover_all/8, 2, sd))
+                "mean" = apply(cover_all/n_years, 2, mean),
+                "sd" = apply(cover_all/n_years, 2, sd))
 library("xtable")
 xtable(df, digits = 3)
+
