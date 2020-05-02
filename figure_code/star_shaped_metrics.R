3014de3d5b0e142f18352653b8c0d9693312938b
Hannah <direch@uw.edu>
2020-05-02 12:37:10 -0700

    made nicer looking

--- figure_code/star_shaped_metrics.R
+++ figure_code/star_shaped_metrics.R
@@ -1,74 +1,101 @@
 rm(list = ls())
-#TO DO: ADD FINDING THE OPTIMAL C EACH EXAMPLE
 library("ContouR")
 library("maptools")
 library("xtable")
-p <- 500
-theta <- seq(0, 2*pi, length = p)
+p <- 200
+theta_space <- 2*pi/p
+thetas <- seq(theta_space/2, 2*pi, theta_space)
+box <- bbox()
+bd <- box@polygons[[1]]@Polygons[[1]]@coords
 
 ###################################
 #Compute metrics for the 4 shapes
 #####################################
 cp_poly <- make_poly(convex_poly, "cp")
-cp <- assess_star(cont = cp_poly, C = c(.5, .5),theta = theta)
+cp_C_under <- cp_C_over <- matrix(data = c(.5, .5), ncol = 2)
+cp <- assess_star(cont = cp_poly, C_under = cp_C_under, C_over = cp_C_over,
+                  thetas = thetas)
+
 cm_poly <- make_poly(convex_mod, "cm")
-cm <- assess_star(cont = cm_poly, C = c(.5, .5),theta = theta)
+cm_C_under <- best_C(bd = bd, conts = cm_poly, thetas = thetas,  area_tol = .001)
+cm_C_over <- best_C(bd = bd, conts = cm_poly, thetas = thetas,  area_tol = .001,
+                    under = FALSE)
+cm <- assess_star(cont = cm_poly, C_under = cm_C_under, C_over = cm_C_over,
+                  thetas = thetas)
+
 circl_poly <- make_poly(circle_mod, "circle")
-circl <- assess_star(cont = circl_poly, C = c(.5, .5),theta = theta)
+circl_C_under <- best_C(bd = bd, conts = circl_poly, thetas = thetas, 
+                        area_tol = .001)
+circl_C_over <- best_C(bd = bd, conts = circl_poly, thetas = thetas, 
+                        area_tol = .001, under = FALSE)
+circl <- assess_star(cont = circl_poly, C_under = circl_C_under, 
+                     C_over = circl_C_over, thetas = thetas)
+
 curl_poly <- make_poly(curl, "circle")
-curl <- assess_star(cont = curl_poly, C = c(.5, .5),theta = theta)
+curl_C_under <- best_C(bd = bd, conts = curl_poly, thetas = thetas, 
+                       area_tol = .001)
+curl_C_over <- best_C(bd = bd, conts = curl_poly, thetas = thetas, 
+                      area_tol = .001, under = FALSE)
+curl <- assess_star(cont = curl_poly, C_under = curl_C_under, 
+                    C_over = curl_C_over, thetas = thetas)
 
-
-###########################
+#########################
 #make figure
 #########################
-pdf("/users/hdirector/desktop/starShapedDemo.pdf")
-par(mfrow = c(4, 3), oma = rep(.2, 4), mar = c(.2, .2, 4, .2))
+pdf("/Users/hdirector/Dropbox/Contours/ContourPaperScripts/figures/starShapedMetric.pdf")
+par(mfrow = c(4, 3), oma = c(0, .5, 2.8, 0), mar = rep(0, 4))
 #ex 1
 plot(cp_poly)
 plot(cp_poly)
-plot(cp$u_error, add = T, col = 'pink')
 plot(cp$u_approx, add = T, border = 'red')
+points(cp_C_under, pch = 3, col = 'red', cex = 2)
 plot(cp_poly)
-plot(cp$o_error, add = T, col = 'lightblue')
 plot(cp$o_approx, add = T, border = 'blue')
+points(cp_C_over, pch = 3, col = 'blue', cex = 2)
 
 #example 2
 plot(cm_poly)
 plot(cm_poly)
 plot(cm$u_error, add = T, col = 'pink')
 plot(cm$u_approx, add = T, border = 'red')
+points(cm_C_under, pch = 3, col = 'red', cex = 2)
 plot(cm_poly)
 plot(cm$o_error, add = T, col = 'lightblue')
 plot(cm$o_approx, add = T, border = 'blue')
+points(cm_C_over, pch = 3, col = 'blue', cex = 2)
 
 #example 3
 plot(circl_poly)
 plot(circl_poly)
 plot(circl$u_error, add = T, col = 'pink')
 plot(circl$u_approx, add = T, border = 'red')
+points(circl_C_under, pch = 3, col = 'red', cex = 2)
 plot(circl_poly)
 plot(circl$o_error, add = T, col = 'lightblue')
 plot(circl$o_approx, add = T, border = 'blue')
+points(circl_C_over, pch = 3, col = 'blue', cex = 2)
 
 #example 4
 plot(curl_poly)
 plot(curl_poly)
 plot(curl$u_error, add = T, col = 'pink')
-plot(curl$u_approx, add = T, border = 'red')
+points(curl_C_under, pch = 3, col = 'red')
+plot(curl$u_approx, add = T, border = 'red', cex = 2)
 plot(curl_poly)
 plot(curl$o_error, add = T, col = 'lightblue')
 plot(curl$o_approx, add = T, border = 'blue')
+points(curl_C_over, pch = 3, col = 'blue', cex = 2)
 
-mtext(text = "Contour", side = 3, line = -3, at = .18, outer = TRUE)
-mtext(text = "Under Approx \n Error Area", side = 3, line = -3, at = .5, 
+#labels
+mtext(text = "Contour", side = 3, line = 0, at = .18, outer = TRUE)
+mtext(text = "Under Approx.", side = 3, line = 0, at = .5, 
       outer = TRUE)
-mtext(text = "Over Approx. \n Error Area", side = 3, line = -3, at = .82,
+mtext(text = "Over Approx.", side = 3, line = 0, at = .82,
       outer = TRUE)
-mtext(text = "Contour 1", side = 2, line = -3, at = .85, outer = TRUE)
-mtext(text = "Contour 2", side = 2, line = -3, at = .58, outer = TRUE)
-mtext(text = "Contour 3", side = 2, line = -3, at = .35, outer = TRUE)
-mtext(text = "Contour 4", side = 2, line = -3, at = .1, outer = TRUE)
+mtext(text = "Contour 1", side = 2, line = -2, at = .89, outer = TRUE)
+mtext(text = "Contour 2", side = 2, line = -2, at = .63, outer = TRUE)
+mtext(text = "Contour 3", side = 2, line = -2, at = .38, outer = TRUE)
+mtext(text = "Contour 4", side = 2, line = -2, at = .1, outer = TRUE)
 dev.off()
 
 ######################
@@ -80,4 +107,4 @@ prop_areas <- matrix(nrow = 4, ncol = 2, byrow = TRUE,
                                circl$u_area/circl$tot_area, circl$o_area/circl$tot_area,
                                curl$u_area/curl$tot_area, curl$o_area/circl$tot_area))
 per_areas <- 100*prop_areas
-xtable(per_areas, digits = 3)
+xtable(per_areas, digits = 2)
